64cd64ae7df600b4b1d791c3a2a5a286c5e05fd6
ChavesSWDev <contactrenchaves@gmail.com>
2023-11-27 23:52:21 -0300

    agendamento funcionando

--- components/Agendar.js
+++ components/Agendar.js
@@ -1,19 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import {
-    Alert,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-} from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Alert, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import * as Style from '../assets/styles';
+import NavBar from './NavBar';
 import { Picker } from '@react-native-picker/picker';
 import * as SQLite from 'expo-sqlite';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 const db = SQLite.openDatabase('BancoLembraAi.db');
+
+
 
 const Agendar = () => {
     const relatedServiceSalaoOptions = [
@@ -22,7 +17,7 @@ const Agendar = () => {
         'Maquiagem',
         'Tratamentos para cabelos',
         'Depilação'
-    ];
+    ]
 
     const relatedServiceOficinaOptions = [
         'Troca de óleo',
@@ -30,7 +25,7 @@ const Agendar = () => {
         'Revisão',
         'Consertos',
         'Instalação de acessórios',
-    ];
+    ]
 
     const relatedServiceBarbeiroOptions = [
         'Corte de cabelo',
@@ -38,15 +33,14 @@ const Agendar = () => {
         'Bigode',
         'Depilação facial',
         'Hidratação capilar',
-    ];
+    ]
 
-    const [agendamentoData, setAgendamentoData] = useState({
+    const [dados, setDados] = useState({
         Nome: '',
         CNPJ: '',
         Servicos: '',
         Logotipo: '',
     });
-
     const [ramo, setRamo] = useState('');
     const [selectedService, setSelectedService] = useState('');
     const [nomeCliente, setNomeCliente] = useState('');
@@ -54,13 +48,10 @@ const Agendar = () => {
     const [data, setData] = useState('');
     const [horario, setHorario] = useState('');
     const navigation = useNavigation();
-    const [serviceOptions, setServiceOptions] = useState([]);
+    const [serviceOptionss, setServiceOptionss] = useState([]);
     const [agendamentos, setAgendamentos] = useState([]);
-    const [status, setStatus] = useState('Aguardando');
-    const [telefoneClienteError, setTelefoneClienteError] = useState('');
-    const [dataError, setDataError] = useState('');
-    const [horarioError, setHorarioError] = useState('');
-    const [campoError, setCampoError] = useState('');
+    const [status, setStatus] = useState("Aguardando")
+
     const route = useRoute();
     const { appointment } = route.params;
 
@@ -75,76 +66,57 @@ const Agendar = () => {
 
     const handleAgendar = () => {
         if (!nomeCliente || !telefoneCliente || !data || !horario || !selectedService) {
-            Alert.alert('Erro', 'Por favor, preencha todos os campos.');
+            console.log('Por favor, preencha todos os campos.');
             return;
         }
-        const handleAgendar = () => {
-            if (!nomeCliente || !telefoneCliente || !data || !horario || !selectedService) {
-                console.log('Por favor, preencha todos os campos.');
-                return;
-            }
 
-            const checkExistingSql = `
-          SELECT COUNT(*) as count
-          FROM Agendamento
-          WHERE Data = ? AND Horario = ?
+        // Verifica se já existe um agendamento para a mesma data e horário
+        const checkExistingSql = `
+            SELECT COUNT(*) as count
+            FROM Agendamento
+            WHERE Data = ? AND Horario = ?
         `;
 
-            const checkExistingParams = [data, horario];
+        const checkExistingParams = [data, horario];
 
-            db.transaction((tx) => {
-                tx.executeSql(
-                    checkExistingSql,
-                    checkExistingParams,
-                    (_, result) => {
-                        const count = result.rows.item(0).count;
+        db.transaction((tx) => {
+            tx.executeSql(
+                checkExistingSql,
+                checkExistingParams,
+                (_, result) => {
+                    const count = result.rows.item(0).count;
 
-                        if (count > 0) {
-                            console.log('Já existe um agendamento para esta data e horário.');
-                            Alert.alert('Aviso', 'Já existe um agendamento para esta data e horário.');
-                        } else {
-                            const insertSql = `
-                  INSERT INTO Agendamento (Nome, Telefone, Data, Horario, Servicos, Status)
-                  VALUES (?, ?, ?, ?, ?, ?)
-                `;
+                    if (count > 0) {
+                        console.log('Já existe um agendamento para esta data e horário.');
+                        Alert.alert('Aviso', 'Já existe um agendamento para esta data e horário.');
+                    } else {
+                        // Se não houver agendamento, procede com a inserção
+                        const insertSql = `
+                            INSERT INTO Agendamento (Nome, Telefone, Data, Horario, Servicos, Status)
+                            VALUES (?, ?, ?, ?, ?, ?)
+                        `;
 
-                            const insertParams = [
-                                nomeCliente,
-                                telefoneCliente,
-                                data,
-                                horario,
-                                selectedService,
-                                status,
-                            ];
+                        const insertParams = [nomeCliente, telefoneCliente, data, horario, selectedService, status];
 
-                            tx.executeSql(
-                                insertSql,
-                                insertParams,
-                                (_, result) => {
-                                    console.log('Dado cadastrado com sucesso!', result);
-                                },
-                                (_, error) => {
-                                    console.error('Erro ao cadastrar os dados!', error);
-                                }
-                            );
+                        tx.executeSql(
+                            insertSql,
+                            insertParams,
+                            (_, result) => {
+                                console.log('Dado cadastrado com sucesso!', result);
+                            },
+                            (_, error) => {
+                                console.error('Erro ao cadastrar os dados!', error);
+                            }
+                        );
 
-                            navigation.navigate('MainMenu');
-                        }
-                    },
-                    (_, error) => {
-                        console.error('Erro ao verificar agendamento existente!', error);
+                        navigation.navigate('MainMenu');
                     }
-                );
-            });
-        };
-    };
-
-    const handleNomeClienteChange = (text) => {
-        if (!text) {
-            Alert.alert('Erro', 'Por favor, preencha o nome do cliente.');
-        } else {
-            setNomeCliente(text);
-        }
+                },
+                (_, error) => {
+                    console.error('Erro ao verificar agendamento existente!', error);
+                }
+            );
+        });
     };
 
     const handleLimpar = () => {
@@ -152,13 +124,12 @@ const Agendar = () => {
         setTelefoneCliente('');
         setData('');
         setHorario('');
-        setSelectedService('Selecione um serviço');
-    };
+        setSelectedService('Selecione um serviço')
+    }
 
     const handleVoltar = () => {
-        navigation.navigate('MainMenu');
-    };
-
+        navigation.navigate('MainMenu')
+    }
 
     useEffect(() => {
         async function buscarDados() {
@@ -168,18 +139,19 @@ const Agendar = () => {
                         'SELECT * FROM Servico',
                         [],
                         (_, resultado) => {
+                            // Processar os resultados aqui
                             console.log('Dados recuperados com sucesso:', resultado);
 
                             let options = [];
                             if (resultado.rows.length > 0) {
                                 for (let i = 0; i < resultado.rows.length; i++) {
                                     const registro = resultado.rows.item(i);
-                                    options.push(registro['Nome']);
+                                    options.push(registro["Nome"]);
                                 }
                             }
 
-                            setServiceOptions(options); // Correção do nome da variável aqui
-                            console.log(serviceOptions);
+                            setServiceOptionss(options); // Atualize o estado com os dados do serviço
+                            console.log(serviceOptionss)
                         },
                         (_, erro) => {
                             console.error('Erro ao recuperar dados:', erro);
@@ -195,136 +167,8 @@ const Agendar = () => {
         buscarDados();
     }, []);
 
-
-    const handleTelefoneChange = (text) => {
-        const formattedText = text.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
-
-        // Verifica se o tamanho do número de telefone está incorreto
-        if (formattedText.length < 11 || formattedText.length > 1) {
-            setTelefoneClienteError('Formato inválido de telefone');
-        } else {
-            setTelefoneClienteError('');
-        }
-
-        setTelefoneCliente(formattedText);
-    };
-
-    const formatarTelefone = (input) => {
-        let output = input.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
-    
-        // Verifica se o número excede o limite máximo de caracteres para cada formato
-        if (output.length > 14) {
-            output = output.slice(0, 14); // Limita o número de caracteres
-        }
-    
-        // Adiciona formatação ao número de telefone
-        if (output.length >= 2 && output.length <= 5) {
-            output = `(${output.slice(0, 2)}) ${output.slice(2)}`;
-        } else if (output.length >= 6 && output.length <= 10) {
-            output = `(${output.slice(0, 2)}) ${output.slice(2, 6)}-${output.slice(6)}`;
-        } else if (output.length >= 11 && output.length <= 14) {
-            output = `+${output.slice(0, 2)} (${output.slice(2, 3)}) ${output.slice(3, 7)}-${output.slice(7)}`;
-        }
-    
-        return output;
-    };
-    
-    const handleDataChange = (text) => {
-        let formattedDate = text.replace(/\D/g, ''); // Remove caracteres não numéricos
-
-        // Limita a entrada a um máximo de 10 caracteres (DD/MM/AAAA)
-        if (formattedDate.length > 8) {
-            formattedDate = formattedDate.slice(0, 8);
-        }
-
-        // Adiciona a barra automaticamente ao digitar a data (DD/MM/AAAA)
-        if (formattedDate.length > 2) {
-            formattedDate = `${formattedDate.slice(0, 2)}/${formattedDate.slice(2)}`;
-        }
-        if (formattedDate.length > 5) {
-            formattedDate = `${formattedDate.slice(0, 5)}/${formattedDate.slice(5)}`;
-        }
-
-        // Verifica se a data é válida e atualiza o estado 'dataError' conforme necessário
-        const { valor, valido } = formatarData(formattedDate);
-        if (!valido) {
-            setDataError('Data inválida');
-        } else {
-            setDataError('');
-        }
-
-        // Atualiza o estado 'data' com a data formatada (corrigida ou não)
-        setData(valido ? formattedDate : valor);
-    };
-    const handleHorarioChange = (text) => {
-        let formattedTime = text.replace(/\D/g, ''); // Remove caracteres não numéricos
-    
-        // Adiciona os dois pontos automaticamente ao digitar o horário (HH:MM)
-        if (formattedTime.length > 2) {
-            formattedTime = `${formattedTime.slice(0, 2)}:${formattedTime.slice(2)}`;
-        }
-    
-        // Remove o último caractere sobressalente se o formato exceder "HH:MM"
-        if (formattedTime.length > 5) {
-            formattedTime = formattedTime.slice(0, 5);
-        }
-
-        // Verifica se o formato tem menos de 5 caracteres (menor que HH:MM)
-        if (formattedTime.length < 5) {
-            setHorarioError('Horário inválido');
-        } else {
-            setHorarioError('');
-        }
-    
-        // Verifica se o formato final do horário está incorreto
-        const { valor, valido } = formatarHora(formattedTime);
-    
-        // Atualiza o estado 'horario' com o horário formatado (corrigido ou não)
-        setHorario(valido ? formattedTime : valor);
-    };
-    
-
-
-
-
-    const formatarHora = (input) => {
-        let output = input.replace(/\D/g, ''); // Remove qualquer caractere não numérico
-        output = output.replace(/^(\d{2})(\d)/g, '$1:$2'); // Adiciona os dois pontos após os primeiros dois dígitos
-
-        // Verifica se a hora é válida
-        const [hora, minuto] = output.split(':').map(Number);
-        if (hora < 0 || hora > 23 || minuto < 0 || minuto > 59) {
-            return { valor: '', valido: false }; // Retorna uma string vazia se a hora não for válida
-        }
-
-        return { valor: output.slice(0, 5), valido: true }; // Limita o tamanho da hora para 'hh:mm'
-    };
-
-
-    const formatarData = (input) => {
-        let output = input.replace(/\D/g, ''); // Remove qualquer caractere não numérico
-        output = output.replace(/^(\d{2})(\d)/g, '$1/$2'); // Adiciona a barra após os primeiros dois dígitos
-        output = output.replace(/(\d{2})(\d)/, '$1/$2'); // Adiciona a barra após os próximos dois dígitos
-
-        // Verifica se a entrada tem o tamanho de uma data (DD/MM/AAAA) completa
-        if (output.length === 10) {
-            const [dia, mes, ano] = output.split('/').map(Number);
-            const data = new Date(ano, mes - 1, dia); // -1 no mês porque no JS os meses começam do zero (janeiro é 0)
-
-            if (dia !== data.getDate() || mes - 1 !== data.getMonth() || ano !== data.getFullYear()) {
-                return { valor: '', valido: false }; // Retorna a data vazia e inválida se não for uma data correta
-            }
-
-            return { valor: output, valido: true }; // Retorna a data formatada apenas se for válida
-        }
-
-        return { valor: output, valido: false }; // Retorna a entrada atualizada para permitir edição
-    };
-
-
     return (
         <ScrollView>
-
             <View style={styles.container}>
                 <Text style={styles.label}>Nome:</Text>
                 <View style={styles.inputContainer}>
@@ -332,79 +176,45 @@ const Agendar = () => {
                         style={styles.input}
                         placeholder="Nome do cliente"
                         value={nomeCliente}
-                        onChangeText={handleNomeClienteChange} />
+                        onChangeText={(text) => setNomeCliente(text)} />
                 </View>
 
                 <Text style={styles.label}>Telefone:</Text>
                 <View style={styles.inputContainer}>
                     <TextInput
-                        style={[
-                            styles.input,
-                            telefoneClienteError && { borderColor: 'red' } // Altera a borda se houver um erro
-                        ]}
+                        style={styles.input}
                         placeholder="Telefone do cliente"
-                        value={formatarTelefone(telefoneCliente)}
-                        onChangeText={handleTelefoneChange}
-                    />
+                        value={telefoneCliente}
+                        onChangeText={(text) => setTelefoneCliente(text)} />
                 </View>
-                {telefoneClienteError ? (
-                    <Text style={{ color: 'red', marginLeft: 76 }}>{telefoneClienteError}</Text>
-                ) : null}
-
-
 
                 <Text style={styles.label}>Data:</Text>
-                <View style={[
-                    styles.inputContainer,
-                    dataError && { borderColor: 'red' }
-                ]}>
+                <View style={styles.inputContainer}>
                     <TextInput
-                        style={[
-                            styles.input,
-                            dataError && { color: 'red' }
-                        ]}
-                        placeholder="DD/MM/AAAA"
-                        value={formatarData(data).valor}
-                        onChangeText={handleDataChange}
-                    />
+                        style={styles.input}
+                        placeholder="Data do agendamento"
+                        value={data}
+                        onChangeText={(text) => setData(text)} />
                 </View>
-                {dataError ? (
-                    <Text style={{ color: 'red', marginLeft: 76 }}>{dataError}</Text>
-                ) : null}
-
-
 
                 <Text style={styles.label}>Horário:</Text>
-                <View style={[
-                    styles.inputContainer,
-                    horarioError && { borderColor: 'red' }
-                ]}>
+                <View style={styles.inputContainer}>
                     <TextInput
-                        editable={true}
-                        style={[
-                            styles.input,
-                            horarioError && { color: 'red' }
-                        ]}
+                        style={styles.input}
                         placeholder="Horário do agendamento"
-                        value={formatarHora(horario).valor}
-                        onChangeText={handleHorarioChange}
-                    />
+                        value={horario}
+                        onChangeText={(text) => setHorario(text)} />
                 </View>
-                {horarioError ? (
-                    <Text style={{ color: 'red', marginLeft: 76 }}>{horarioError}</Text>
-                ) : null}
-
-
 
                 <Text style={styles.label}>Serviços:</Text>
                 <View style={styles.pickerContainer}>
                     <Picker
-                        style={styles.picker}
+                        style={styles.Picker}
                         selectedValue={selectedService}
                         onValueChange={(itemValue) => setSelectedService(itemValue)}
                     >
                         <Picker.Item label="Selecione um serviço" value="" />
-                        {serviceOptions.map((service, index) => (
+                        {serviceOptionss.map((service, index) => (
                             <Picker.Item key={index} label={service} value={service} />
                         ))}
                     </Picker>
@@ -428,6 +238,7 @@ const Agendar = () => {
         </ScrollView>
     )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -534,4 +345,5 @@ const styles = StyleSheet.create({
     },
 
 });
-export default Agendar;
+
+export default Agendar
